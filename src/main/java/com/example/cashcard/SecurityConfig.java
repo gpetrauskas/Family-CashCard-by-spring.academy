0c6fccb3687c5d131273b91cd2538c1a99202bcb
gpetrauskas <inconoir@gmail.com>
2023-12-01 13:54:14 +0200

    Update SecurityConfig to include access control for /cashcards and provide test users

--- src/main/java/com/example/cashcard/SecurityConfig.java
+++ src/main/java/com/example/cashcard/SecurityConfig.java
@@ -2,9 +2,14 @@ package com.example.cashcard;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -12,7 +17,28 @@ public class SecurityConfig {
 
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(request -> request
+                .requestMatchers("/cashcards/**")
+                .hasRole("CARD-OWNER"))
+                .csrf(csrf -> csrf.disable())
+                .httpBasic(Customizer.withDefaults());
         return http.build();
+    }
+
+    @Bean
+    UserDetailsService testOnlyUsers(PasswordEncoder passwordEncoder) {
+        User.UserBuilder users = User.builder();
+        UserDetails sarah = users
+                .username("sarah1")
+                .password(passwordEncoder.encode("abc123"))
+                .roles("CARD-OWNER").build();
+
+        UserDetails hank = users
+                .username("hank")
+                .password(passwordEncoder.encode("qwe123"))
+                .roles("NOT-CARD-OWNER").build();
+
+        return new InMemoryUserDetailsManager(sarah, hank);
     }
 
     @Bean
