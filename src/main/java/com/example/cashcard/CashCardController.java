a645fc2efc444fd342c275f88c9394cdc3c300f1
gpetrauskas <inconoir@gmail.com>
2023-12-05 10:44:36 +0200

    Added deleteCashCard endpoint, with no logic, returning 204 No Content for now

--- src/main/java/com/example/cashcard/CashCardController.java
+++ src/main/java/com/example/cashcard/CashCardController.java
@@ -66,6 +66,11 @@ class CashCardController {
         return ResponseEntity.notFound().build();
     }
 
+    @DeleteMapping("/{id}")
+    private ResponseEntity<Void> deleteCashCard(@PathVariable Long id) {
+        return ResponseEntity.noContent().build();
+    }
+
     private CashCard findCashCard(Long requestedId, Principal principal) {
         return cashCardRepository.findByIdAndOwner(requestedId, principal.getName());
     }
