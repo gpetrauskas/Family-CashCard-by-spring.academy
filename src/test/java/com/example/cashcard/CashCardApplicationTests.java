2ea9546eb7e2d6db841a6a18f0d970253b666fe7
gpetrauskas <inconoir@gmail.com>
2023-12-05 12:09:45 +0200

    Added a test that should return NOT FOUND if cashcard that does not exist is trying to be deleted

--- src/test/java/com/example/cashcard/CashCardApplicationTests.java
+++ src/test/java/com/example/cashcard/CashCardApplicationTests.java
@@ -231,8 +231,17 @@ class CashCardApplicationTests {
 		ResponseEntity<String> getResponse = restTemplate
 				.withBasicAuth("sarah1", "abc123")
 				.getForEntity("/cashcards/99", String.class);
-		
+
 		assertThat(getResponse.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+	}
+
+	@Test
+	void shouldNotDeleteACashCardThatDoesNotExist() {
+		ResponseEntity<Void> deleteResponse = restTemplate
+				.withBasicAuth("sarah1", "abc123")
+				.exchange("/cashcards/99", HttpMethod.DELETE, null, Void.class);
+
+		assertThat(deleteResponse.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
 	}
 
 }
